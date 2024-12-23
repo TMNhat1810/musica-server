462a355fad19500f460994a21b2d71aa2419e3b5
TMNhat1810 <minhnhat912002@gmail.com>
2024-12-23 18:50:54 +0700

    feat: add db connection and prisma orm

---
+++ src/modules/database/services/index.ts
@@ -0,0 +1,4 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { PrismaService } from './prisma.service';
+
+export { PrismaService, PrismaClientKnownRequestError };
